d2585b37bf3d21522584f60d85349b84328ffd89
xionghaoo <xionghao930316@gmail.com>
2021-11-26 00:03:57 +0800

    .gitignore fix

--- common/build/tmp/kapt3/stubs/debug/com/example/android/uamp/media/library/AlbumArtContentProviderKt.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.uamp.media.library;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\t\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"DOWNLOAD_TIMEOUT_SECONDS", "", "common_debug"})
-public final class AlbumArtContentProviderKt {
-    public static final long DOWNLOAD_TIMEOUT_SECONDS = 30L;
-}
